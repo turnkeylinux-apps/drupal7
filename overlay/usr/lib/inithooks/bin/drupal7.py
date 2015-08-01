9f63c2117e5080417dbd17e16c995d25b945b681
Dude4Linux <dude4linux@gmail.com>
2015-08-01 20:35:07 +0000

    changed references to drush path consistent with 22a49fd

--- overlay/usr/lib/inithooks/bin/drupal7.py
+++ overlay/usr/lib/inithooks/bin/drupal7.py
@@ -57,9 +57,9 @@ def main():
     m = MySQL()
     m.execute('UPDATE drupal7.users SET mail=\"%s\" WHERE name=\"admin\";' % email)
     m.execute('UPDATE drupal7.users SET init=\"%s\" WHERE name=\"admin\";' % email)
-    system("/usr/bin/drush variable-set site_mail %s" % email)
-    system("/usr/bin/drush variable-set update_notify_emails %s" % email)
-    system("/usr/bin/drush user-password admin --password='%s'" % password)
+    system("/usr/local/bin/drush variable-set site_mail %s" % email)
+    system("/usr/local/bin/drush variable-set update_notify_emails %s" % email)
+    system("/usr/local/bin/drush user-password admin --password='%s'" % password)
 
 if __name__ == "__main__":
     main()
